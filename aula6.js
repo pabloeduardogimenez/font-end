309af7ec954d03574e297ec3df2aeead76031c39
pabloeduardogimenez <pabloedug7@gmail.com>
2025-02-25 20:05:50 -0300

    add aula6.js

---
+++ aula6.js
@@ -0,0 +1,20 @@
+function soma(n1, n2){
+    var resultado = n1 + n2;
+    return resultado;
+}
+soma(2,3);
+
+function oi(){            
+    var  valor = texto.value;
+    mensagem.innerText = valor;
+
+}
+
+function maiusculas(){            
+    var  valor = texto.value;
+    toUpperCase(valor);
+    mensagem.innerText =valor.toUpperCase();
+
+ 
+
+}
