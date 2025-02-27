1f6bfddef45ec22f661b77f59a7ac46113da5004
pabloeduardogimenez <pabloedug7@gmail.com>
2025-02-27 20:14:01 -0300

    mercado Livre Rodrigo

--- exercicios.js
+++ exercicios.js
@@ -64,30 +64,61 @@ function notas(){
         console.log("aprovado");
     }
 }
-
+//ini=+ 1
 //Fibonacci
 // 0 1  1  2 3 5 8 
 // vai dizer numeros da sequencia 
 // 8 
 function fibo(){
     var num = valor.value; 
-    var soma ;
-    var numero1 = 1;
-    var numero2 = 1;  
-    console.log( "fibonacci =  " +  num  );
-    console.log(  "0" );
-    console.log(  "1" );
-    console.log(  "1" );
+    if (num = 1){
+        console.log( "fibonacci =  " +  num  );
+        console.log(  "0" );        
+    } else if ( um = 2){
+        console.log( "fibonacci =  " +  num  );
+        console.log(  "0" );
+        console.log(  "1" );
 
-    for (var ini = 1 ; ini <= num ; ini+= 1 )
-    {
-         
-        soma = numero1 + numero2;
-        numero2 = numero1;
-        numero1 = soma;        
+    } else if (um > 2)
+    {  
+            var soma ;
+            var numero1 = 1;
+            var numero2 = 1;  
+            console.log( "fibonacci =  " +  num  );
+            console.log(  "0" );
+            console.log(  "1" );
+            console.log(  "1" );
 
-        console.log(  soma );
+            for (var ini = 1 ; ini <= num ; ini+= 1 )
+            {
+                
+                soma = numero1 + numero2;
+                numero2 = numero1;
+                numero1 = soma;        
 
+                console.log(  soma );
+
+            }
     }
 
+}
+function fibo2(){
+    var num = valor.value;
+    var n1 = 0;
+    var n2 = 1;
+    if ( num >= 2 )
+    {
+    console.log(n1);
+    console.log(n2);
+    } else 
+    if (num == 1){
+        console.log(n1);
+    }
+    while (num >2){
+        var n3 = n1 + n2;
+        console.log(n3);
+        n1 = n2;
+        n2 = n3;
+        num--;
+    }
 }
