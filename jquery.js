a56b4a9625edc0cc09d2b3055f3d87a279a19c37
pabloeduardogimenez <pabloedug7@gmail.com>
2025-02-27 21:42:43 -0300

    funções jquery

---
+++ jquery.js
@@ -0,0 +1,36 @@
+function mudar()
+{
+    var cor = $("#cor").val();
+    $(".titulo").css("color", cor);
+}
+
+function add()
+{   
+  
+    var cidade = $("#cidade").val();
+    var primeira  = cidade.charAt(0);
+    var resto = cidade.Substring(1,0);
+    var novacidade = primeira.toUpperCase() + resto.toLowerCase();   
+
+    if ( $("#lista").html.includes(cidade) == true )
+    {
+       alert("cidade já cadastrade");
+    }
+    else {
+        $("#lista").append("<li>" + cidade + "</li>");    }
+   
+    $("#cidade").val(""); 
+
+}
+
+$(document).ready(function(){
+    $(".bt-add").click(add);
+    $(".titulo").css("color", "red");
+
+    $("h1" , "li").clik(function(){
+        var nova = $("#cor").val();
+        $(this).css("color", nova);
+    });
+
+
+});
