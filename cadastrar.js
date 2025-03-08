4c28f83f40c868c3952dacb2bed6a46f922b8a7a
pabloeduardogimenez <pabloedug7@gmail.com>
2025-03-08 09:15:49 -0300

    comin

--- cadastrar.js
+++ cadastrar.js
@@ -1,4 +1,21 @@
-$(document).reayd(function(){
+$(document).ready(function(){
+   $(".bt-add").click(
+        function()
+        {
+            var nome = $("#nome").val();
+
+            $("#lista").append('<li class="list-group-item">'+nome+'</li>');
+        }); // fim bt-add 
+
+       
+        $(".bt-del").click(
+            function()
+            {
+                var lista = $("#lista").val();
+                var n = lista.indexOf + 1 ;
+                
+                $("#lista").splice(n, 1);
+                $("#lista").append('<li class="list-group-item">'+ n +'</li>');
+            }); // fim bt-del 
     
-    
-})
+});
